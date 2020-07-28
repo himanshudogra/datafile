c451c1c42fddd1c88457fdb6dc8034fc5751e6e1
himanshu <himanshudogra96@gmail.com>
2020-07-28 11:35:14 +0530

    Update int.go

--- slices/int/int.go
+++ slices/int/int.go
@@ -13,25 +13,25 @@ func GetInt(fileName string) ([]int, error) {
 
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		number, err := strconv.Atoi(scanner.Text())
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 	}
 
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 	return numbers, nil
 }
